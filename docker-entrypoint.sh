145fb877fd516f20b6d783582caaa53154a3c642
Giovanni Ferrara <>
2018-06-14 14:46:57 +0200

    gosu not working

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -34,7 +34,7 @@ if [ -z "$PROJECT" ]; then
 fi
 
 if [ -z "$MOCK_SERVICE_NAME" ]; then
-    echo "Enviromentment variable MOCK_SERVICE_NAME should have been set explicitly (e.g. by  -e MOCK_SERVICE_NAME=BLZ-SOAP11-MockService"
+    echo "Enviromentment variable MOCK_SERVICE_NAME should have been set explicitly (e.g. by  -e MOCK_SERVICE_NAME=GisServerBindingMock"
     exit 1
 fi
 
@@ -59,15 +59,15 @@ if [ "$1" = 'start-soapui' ]; then
 
     if [ -z "$MOCK_SERVICE_PATH" ]; then
         echo "Starting Mock-service=$MOCK_SERVICE_NAME using default mockservice url-path from SoapUI-project=$PROJECT"
-        gosu soapui mockservicerunner.sh -Djava.awt.headless=true -p 8080 -m $MOCK_SERVICE_NAME $PROJECT <&3 &
+        mockservicerunner.sh -Djava.awt.headless=true -p $MOCK_SERVICE_PORT -m $MOCK_SERVICE_NAME $PROJECT <&3 &
     else
         echo "Starting Mock-service=$MOCK_SERVICE_NAME using url-path=$MOCK_SERVICE_PATH from SoapUI-project=$PROJECT"
-        gosu soapui mockservicerunner.sh -Djava.awt.headless=true -p 8080 -m $MOCK_SERVICE_NAME -a $MOCK_SERVICE_PATH $PROJECT <&3 &
+        mockservicerunner.sh -Djava.awt.headless=true -p $MOCK_SERVICE_PORT -m $MOCK_SERVICE_NAME -a $MOCK_SERVICE_PATH $PROJECT <&3 &
     fi
 
 else
     echo "You can start the Mock-service manually by running"
-    echo ">>>  mockservicerunner.sh -Djava.awt.headless=true -p 8080 -m $MOCK_SERVICE_NAME $PROJECT"
+    echo ">>>  mockservicerunner.sh -Djava.awt.headless=true -p $MOCK_SERVICE_PORT -m $MOCK_SERVICE_NAME $PROJECT"
     gosu soapui "$@" <&3 &
 fi
 
